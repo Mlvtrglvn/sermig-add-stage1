46bb966882078ab3c3966b89f52c9f0bd5f5734d
Dario Bertero <anrvrx@gmail.com>
2013-06-21 16:25:41 +0200

    Added conversion expression for lm35 class
    Conversion expression taken from http://playground.arduino.cc/Main/LM35HigherResolution

--- source/lm35.ino
+++ source/lm35.ino
@@ -10,17 +10,23 @@ TemperatureLm35::TemperatureLm35(int pin_p) : Sensor(pin_p, true) {}
 /*!
 	Trigger the sensor, reading and converting its data.
 
+	Uses the conversion method of http://playground.arduino.cc/Main/LM35HigherResolution
+
 	\return true if the data is valid
 */
 bool TemperatureLm35::trigger() 
 {
 	unsigned int valueRead;
 
+	analogReference(INTERNAL); // Raise the resolution of the lm35 sensor switching to 1.1V as reference of the analog pin
 	valueRead = analogRead(pin); // No impedence at the pin, no need to wait.
+	analogReference(DEFAULT); // Reset the reference change
 	
-	lastValue = (double) valueRead; // TODO: write the conversion expression
+	// Conversion expression taken from http://playground.arduino.cc/Main/LM35HigherResolution (1C = 9.31 analog reading)
+	lastValue = (double) valueRead / 9.31;
 	valid = true;
 
+	// Data output to serial
 	Serial.print("TEMP,");
 	Serial.print(lastValue);
 	Serial.print("\n");
